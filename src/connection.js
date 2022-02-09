29a18b25bbad46cc111998e5f4e1fe2da7ff9244
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-09 19:21:14 -0300

    criando conexão com banco de dados

---
+++ src/connection.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const knex = require("knex")({
+  client: "pg",
+  connection: {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+  },
+});
+
+module.exports = knex;
