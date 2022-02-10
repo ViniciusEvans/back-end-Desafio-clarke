0cb588d3fe7946d575d803404974db1e2bed29fe
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-09 21:04:11 -0300

    subtraindo 1 no limit

--- src/controllers/interprises.js
+++ src/controllers/interprises.js
@@ -11,12 +11,17 @@ const interprises = {
 
     try {
       const response = await knex("interprises")
-        .limit(5)
+        .limit(4)
         .offset(offset)
         .where("min_demand", ">", Number(energy_demand))
         .orderBy("min_demand", "asc")
         .returning("*");
       console.log(response);
+      if (response.length === 0) {
+        res
+          .status(404)
+          .json({ mensagem: "Não há mais empresas em nosso banco de dados" });
+      }
       res.status(200).json(response);
     } catch (error) {
       res.status(500).json(error.message);
